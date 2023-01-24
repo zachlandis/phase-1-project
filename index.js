d9a91d97ad63f9e2477ba5ea507d72bee2a7ee60
Zach <zachlandis91@gmail.com>
2023-01-24 12:03:49 -0700

    Fix likeCounter, create form

--- index.js
+++ index.js
@@ -10,15 +10,39 @@ document.addEventListener('DOMContentLoaded', () => {
         let setup = document.createElement('h3')
         setup.textContent = `${joke.setup}`
 
+        let delivery = document.createElement('p')
+
+        let deliveryBtn = document.createElement('button')
+        deliveryBtn.textContent = "Punchline"
+        deliveryBtn.addEventListener('click', () => delivery.append(joke.delivery))
+
+        let likes = document.createElement('p')
+        likes.textContent = `Likes: ${joke.likes}`
+
         let likeBtn = document.createElement('button')
         likeBtn.textContent = "Like"
-        likeBtn.addEventListener('click', (e) => {increaseLikes(joke)})
+        likeBtn.addEventListener('click', () => {
+            let likeCounter = joke.likes
+            likeCounter = joke.likes ++
+            fetch(`http://localhost:3000/jokes/${joke.id}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify({
+                    "likes": joke.likes
+                })
+            })
+            likes.textContent = `Likes: ${joke.likes}`
+
+        })
+
+
 
         document.querySelector('#joke_container').append(card)
-        card.append(setup, likeBtn)   
+        card.append(setup, delivery, deliveryBtn, likeBtn, likes)   
     }
 })
 
-function increaseLikes(joke) {
-    console.log(joke)
-}
+
