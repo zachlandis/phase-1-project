1a66372f110ef448f8c0d4378bd696066fd528a1
Zach <zachlandis91@gmail.com>
2023-01-24 11:04:56 -0700

    build joke card function

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-fetch ('http://localhost:3000/jokes')
-.then(res => res.json())
-.then(data => data.forEach(joke => buildJokeCards(joke)))
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('http://localhost:3000/jokes')
+    .then(res => res.json())
+    .then(data => {data.forEach(joke => buildJokeCards(joke))})
+
+    function buildJokeCards(joke) {
+        let card = document.createElement('div')
+
+
+
+        let setup = document.createElement('h3')
+        setup.textContent = `${joke.setup}`
+
+        document.querySelector('#joke_container').append(card)
+        card.append(setup)
+        
+    }
+})
+
 
